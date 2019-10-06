9d161d5dc8a5b361590e4477a4bd2823a55b2d05
Jakub Plona <plonajakub@gmail.com>
2019-10-06 20:49:32 +0200

    Add project base files

---
+++ main.cpp
@@ -0,0 +1,13 @@
+#include "tests/MatrixGraphTest.h"
+#include "tests/ListGraphTest.h"
+
+int main() {
+
+    ListGraphTest lgt;
+    lgt.run();
+
+//    MatrixGraphTest mgt;
+//    mgt.run();
+
+    return 0;
+}
