58aa4296a7f740f13a35e74e14b0b9540d0f3d23
Jakub Plona <plonajakub@gmail.com>
2019-12-08 17:17:23 +0100

    Refactor LocalSearchParameters.h

--- algorithms/helper_structures/LocalSearchParameters.h
+++ algorithms/helper_structures/LocalSearchParameters.h
@@ -7,26 +7,30 @@
 class LocalSearchParameters {
 
 public:
-    // simulated annealing
+    // Simulated annealing
     double initialTemperature;
     double coolingSchemeParameter;
     int epochIterationsNumber;
-    int iterationsNumber; // and tabu search
+    int iterationsNumber;
     TSPLocalSearchAlgorithms::fCoolingScheme coolingSchemeFunction;
-    TSPLocalSearchAlgorithms::fNeighbourhood nextNeighbourFunction; // and tabu search
-    TSPGreedyAlgorithms::fTSPAlgorithm initialSolutionFunction; // and tabu search
+    TSPLocalSearchAlgorithms::fNeighbourhood nextNeighbourFunction;
+    TSPGreedyAlgorithms::fTSPAlgorithm initialSolutionFunction;
 
-    // tabu search
+    // Tabu search
+//    int iterationsNumber;
     int tabuListSize;
     double cadenzaLengthParameter;
     int iterationsWithoutImprovementToRestart;
     int patternsNumberToCache;
+//    TSPGreedyAlgorithms::fTSPAlgorithm initialSolutionFunction;
+//    TSPLocalSearchAlgorithms::fNeighbourhood nextNeighbourFunction;
 
     LocalSearchParameters() : initialTemperature(-1), coolingSchemeParameter(-1), epochIterationsNumber(-1),
                               iterationsNumber(-1), coolingSchemeFunction(nullptr), nextNeighbourFunction(nullptr),
                               initialSolutionFunction(nullptr), tabuListSize(-1), cadenzaLengthParameter(-1),
                               iterationsWithoutImprovementToRestart(-1), patternsNumberToCache(-1) {}
 
+    // Simulated annealing
     LocalSearchParameters(double initialTemperature, double coolingSchemeParameter, int epochIterationsNumber,
                           int iterationsNumber, TSPLocalSearchAlgorithms::fCoolingScheme coolingSchemeFunction,
                           TSPLocalSearchAlgorithms::fNeighbourhood nextNeighbourFunction,
@@ -35,6 +39,17 @@ public:
               epochIterationsNumber(epochIterationsNumber), iterationsNumber(iterationsNumber),
               coolingSchemeFunction(coolingSchemeFunction), nextNeighbourFunction(nextNeighbourFunction),
               initialSolutionFunction(initialSolutionFunction) {}
+
+    // Tabu search
+    LocalSearchParameters(int iterationsNumber, int tabuListSize, double cadenzaLengthParameter,
+                          int iterationsWithoutImprovementToRestart, int patternsNumberToCache,
+                          TSPGreedyAlgorithms::fTSPAlgorithm initialSolutionFunction,
+                          TSPLocalSearchAlgorithms::fNeighbourhood nextNeighbourFunction) :
+            iterationsNumber(iterationsNumber), tabuListSize(tabuListSize),
+            cadenzaLengthParameter(cadenzaLengthParameter),
+            iterationsWithoutImprovementToRestart(iterationsWithoutImprovementToRestart),
+            patternsNumberToCache(patternsNumberToCache), initialSolutionFunction(initialSolutionFunction),
+            nextNeighbourFunction(nextNeighbourFunction) {}
 };
 
 
